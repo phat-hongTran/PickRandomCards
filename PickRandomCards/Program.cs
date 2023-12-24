fc39948d6d721292dc10daa0e17872f3aa6ca500
Phat Tran <ptran@strongtie.com>
2023-12-24 12:13:03 +0700

    Finish

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -6,7 +6,21 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (var card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number is not valid.");
+            }
         }
     }
 }
